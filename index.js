1025788f408fae8de4b236630407c0ad6c27612f
Daniel Holz <dholz42@gmail.com>
2016-10-18 14:51:16 -0700

    added more files

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-
+var fs = require("fs");
+var path = require("path");
 
 module.exports = function(cachePath){
-	var path = document.location.pathname;
-    return "The current path is: " + path;
+    // var currentPath = process.cwd();
+    // console.log("The current path is: " + currentPath);
+    //
+    // console.log("Now we need to go to " + cachePath);
+    // process.chdir(cachePath);
+    // console.log("The new directory we are in is: " + process.cwd());
+    //
+    // console.log("Listing files in this directory:");
+
+    var p = cachePath;
+
+    fs.readdir(p, function(err, files){
+        if (err){
+            throw err;
+        }
+
+        files.map(function(file){
+            return path.join(p, file);
+        }).filter(function(file){
+            return fs.statSync(file).isFile();
+        }).forEach(function(file){
+            console.log("%s", file);
+        })
+    })
 };
